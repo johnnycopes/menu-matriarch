c9d7cf1823aac61380cc1f4c61c83842e57ace0f
John Coppola <jdcoppola@gmail.com>
2021-10-24 23:48:54 -0400

    refactors inline edit form toggle to use observables

--- src/app/features/menus/menu/menu.component.ts
+++ src/app/features/menus/menu/menu.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { merge, Subject } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
 
 import { IMenuEntry } from '@models/interfaces/menu-entry.interface';
 import { Day } from '@models/types/day.type';
@@ -10,33 +12,28 @@ import { MenuService } from '@services/menu.service';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
   @Input() id = '';
   @Input() name = '';
   @Input() entries: IMenuEntry[] = [];
   @Input() days: Day[] = [];
   public faEllipsisV = faEllipsisV;
   public renaming = false;
+  public startRename$ = new Subject<void>();
+  public finishRename$ = new Subject<void>();
+  public renaming$ = merge(
+    this.startRename$.pipe(mapTo(true)),
+    this.finishRename$.pipe(mapTo(false)),
+  );
 
-  constructor(
-    private _cdRef: ChangeDetectorRef,
-    private _menuService: MenuService,
-  ) { }
-
-  ngOnInit(): void {
-  }
+  constructor(private _menuService: MenuService) { }
 
   public async onRename(name: string): Promise<void> {
     await this._menuService.updateMenuName(this.id, name);
-    this.toggleRenaming();
+    this.finishRename$.next();
   }
 
   public onDelete(): void {
     this._menuService.deleteMenu(this.id);
-  }
-
-  public toggleRenaming(): void {
-    this.renaming = !this.renaming;
-    this._cdRef.markForCheck();
   }
 }
--- src/app/features/menus/menus.component.ts
+++ src/app/features/menus/menus.component.ts
@@ -1,16 +1,17 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { combineLatest } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { combineLatest, merge, Subject } from 'rxjs';
+import { map, mapTo, shareReplay } from 'rxjs/operators';
 
 import { MenuService } from '@services/menu.service';
 import { MealService } from '@services/meal.service';
+import { ButtonComponent } from '@shared/generic/button/button.component';
 
 @Component({
   selector: 'app-menus',
   templateUrl: './menus.component.html',
   styleUrls: ['./menus.component.scss']
 })
-export class MenusComponent implements OnInit {
+export class MenusComponent {
   public menus$ = combineLatest([
     this._menuService.getOrderedDays(),
     this._menuService.getMenus(),
@@ -23,26 +24,27 @@ export class MenusComponent implements OnInit {
       })
     ))
   );
-  public adding = false;
+  public startAdd$ = new Subject<void>();
+  public finishAdd$ = new Subject<void>();
+  public adding$ = merge(
+    this.startAdd$.pipe(mapTo(true)),
+    this.finishAdd$.pipe(mapTo(false)),
+  ).pipe(
+    shareReplay({ refCount: true, bufferSize: 1 })
+  );
+
+  @ViewChild(ButtonComponent, { read: ElementRef, static: true })
+  public buttonRef: ElementRef<HTMLButtonElement> | undefined;
 
   constructor(
-    private _cdRef: ChangeDetectorRef,
     private _mealService: MealService,
     private _menuService: MenuService,
   ) { }
-
-  ngOnInit(): void {
-  }
 
   public onSave(name: string): void {
     this._menuService
       .createMenu(name)
       .subscribe();
-    this.toggleAdding();
-  }
-
-  public toggleAdding(): void {
-    this.adding = !this.adding;
-    this._cdRef.markForCheck();
+    this.finishAdd$.next();
   }
 }
