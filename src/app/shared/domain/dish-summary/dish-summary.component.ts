583a68d53fec44af88b7c6fe6b75b206274b5237
John Coppola <jdcoppola@gmail.com>
2021-11-05 15:09:20 -0400

    refactors dish-summary to include sides

--- src/app/shared/domain/dish-summary/dish-summary.component.ts
+++ src/app/shared/domain/dish-summary/dish-summary.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 
 import { DishType } from '@models/interfaces/dish-type.type';
+import { IDish } from '@models/interfaces/dish.interface';
 import { UserService } from '@services/user.service';
+import { trackByFactory } from '@shared/utility/track-by-factory';
 
 @Component({
   selector: 'app-dish-summary',
@@ -10,14 +12,27 @@ import { UserService } from '@services/user.service';
   styleUrls: ['./dish-summary.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DishSummaryComponent {
+export class DishSummaryComponent implements OnInit {
   @Input() id: string | undefined;
   @Input() name: string | undefined;
   @Input() description: string | undefined;
   @Input() type: DishType | undefined;
+  @Input() sides: IDish[] | undefined;
+  public trackByFn = trackByFactory<IDish, string>(dish => dish.id);
   public fallbackName$ = this._userService.getPreferences().pipe(
     map(preferences => preferences?.emptyDishText ?? '')
   );
 
+  @HostBinding('class')
+  public get hostClass(): string {
+    return this.description || this.type ? 'vertical' : 'horizontal'
+  }
+
   constructor(private _userService: UserService) { }
+
+  public ngOnInit(): void {
+    if ((this.description || this.type) && this.sides) {
+      throw new Error('app-dish-summary should either be passed a `description` and `type` OR `sides`');
+    }
+  }
 }
