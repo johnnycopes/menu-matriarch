49dc5c092d9b0b11c0b441ec29a619d3bc8fb4e4
John Coppola <jdcoppola@gmail.com>
2021-11-12 00:17:56 -0500

    displays tags in dish summary

--- src/app/shared/domain/dish-summary/dish-summary.component.ts
+++ src/app/shared/domain/dish-summary/dish-summary.component.ts
@@ -1,7 +1,10 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { faMap } from '@fortawesome/free-regular-svg-icons';
 import { faCalendarDay } from '@fortawesome/free-solid-svg-icons';
+
+import { TagService } from '@services/tag.service';
 
 @Component({
   selector: 'app-dish-summary',
@@ -13,9 +16,22 @@ export class DishSummaryComponent {
   @Input() id: string = '';
   @Input() name: string = '';
   @Input() description: string | undefined;
-  @Input() tags: string[] = [];
   @Input() menus: string[] = [];
   @Input() usages: number = 0;
+  @Input()
+  public set tags(value: string[]) {
+    this._tagIds$.next(value);
+  }
+
+  private _tagIds$ = new BehaviorSubject<string[]>([]);
+  public tags$ = combineLatest([
+    this._tagIds$.asObservable(),
+    this._tagService.getTags()
+  ]).pipe(
+    map(([ids, tags]) => tags.filter(tag => ids.includes(tag.id)))
+  );
   public readonly faMap = faMap;
   public readonly faCalendarDay = faCalendarDay;
+
+  constructor(private _tagService: TagService) { }
 }
