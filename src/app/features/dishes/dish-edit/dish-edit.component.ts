0e9ae5f30f69318abd1f176a910ed6f561557548
John Coppola <jdcoppola@gmail.com>
2021-11-13 09:39:16 -0500

    tag add/remove working in dish edit component

--- src/app/features/dishes/dish-edit/dish-edit.component.ts
+++ src/app/features/dishes/dish-edit/dish-edit.component.ts
@@ -1,17 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { of } from 'rxjs';
-import { first, switchMap, tap } from 'rxjs/operators';
+import { combineLatest, of } from 'rxjs';
+import { first, map, switchMap, tap } from 'rxjs/operators';
 
 import { DishType } from '@models/types/dish-type.type';
 import { DishService } from '@services/dish.service';
+import { TagService } from '@services/tag.service';
 
 interface IDishEditForm {
   name: string;
   description: string;
   type: DishType;
+  tags: string[];
 }
+
 
 @Component({
   selector: 'app-dish-edit',
@@ -21,7 +24,9 @@ interface IDishEditForm {
 })
 export class DishEditComponent {
   private _routeId = this._route.snapshot.paramMap.get('id');
-  public dish$ = this._routeId ? this._dishService.getDish(this._routeId) : of(undefined);
+  public dish$ = this._routeId
+    ? this._dishService.getDish(this._routeId)
+    : of(undefined);
   public vm$ = of(this._routeId).pipe(
     switchMap(id => {
       if (!id) {
@@ -29,9 +34,22 @@ export class DishEditComponent {
           name: '',
           description: '',
           type: 'main',
+          tags: [],
         });
       }
-      return this.dish$;
+      return combineLatest([
+        this.dish$,
+        this._tagService.getTags(),
+      ]).pipe(
+        map(([dish, tags]) => ({
+          ...dish,
+          tags: tags.map(tag => ({
+            id: tag.id,
+            name: tag.name,
+            checked: !!dish?.tags.find(dishTag => dishTag.id === tag.id)
+          }))
+        }))
+      );
     })
   );
 
@@ -39,6 +57,7 @@ export class DishEditComponent {
     private _route: ActivatedRoute,
     private _router: Router,
     private _dishService: DishService,
+    private _tagService: TagService,
   ) { }
 
   public async onSave(form: NgForm): Promise<void> {
@@ -46,6 +65,10 @@ export class DishEditComponent {
       name: form.value.name,
       description: form.value.description,
       type: form.value.type,
+      tags: Object
+        .entries<boolean>(form.value.tags)
+        .filter(([key, checked]) => checked)
+        .map(([key, checked]) => key)
     };
     if (!this._routeId) {
       this._dishService.createDish(details).pipe(
