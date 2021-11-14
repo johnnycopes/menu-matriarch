14fd7d496e6c46584cc9c8511e6224f5f780ee7e
John Coppola <jdcoppola@gmail.com>
2021-11-14 09:56:14 -0500

    implements search input on dishes screen

--- src/app/features/planner/planner-dishes/planner-dishes.component.ts
+++ src/app/features/planner/planner-dishes/planner-dishes.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs/operators';
+
 import { IDish } from '@models/interfaces/dish.interface';
 import { DishType } from '@models/types/dish-type.type';
-
 import { DishService } from '@services/dish.service';
 import { lower } from '@shared/utility/format';
 import { trackByFactory } from '@shared/utility/track-by-factory';
-import { BehaviorSubject, combineLatest } from 'rxjs';
-import { debounceTime, map, startWith, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-planner-dishes',
@@ -18,7 +18,9 @@ export class PlannerDishesComponent {
   private _searchText$ = new BehaviorSubject<string>('');
   public vm$ = combineLatest([
     this._dishService.getDishes(),
-    this._searchText$.asObservable(),
+    this._searchText$.asObservable().pipe(
+      distinctUntilChanged(),
+    ),
   ]).pipe(
     map(([dishes, searchText]) => ({
       searchText,
@@ -38,6 +40,7 @@ export class PlannerDishesComponent {
   private _filterDish(dish: IDish, type: DishType, searchText: string): boolean {
     return dish.type === type &&
       lower(dish.name).includes(lower(searchText)) ||
+      lower(dish.description).includes(lower(searchText)) ||
       dish.tags.some(tag => lower(tag.name).includes(lower(searchText)));
   }
 }
