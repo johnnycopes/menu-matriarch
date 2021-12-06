3c146cf2eacf2774b0eea57bf43feb7b0829ada1
John Coppola <jdcoppola@gmail.com>
2021-12-06 15:55:59 -0500

    adds caret icon to filters-button; fixes icon issues in base button component

--- src/app/shared/domain/filters-button/filters-button.component.ts
+++ src/app/shared/domain/filters-button/filters-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy, Input, EventEmitter, Output } from '@angular/core';
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-filters-button',
@@ -10,4 +11,6 @@ export class FiltersButtonComponent {
   @Input() count = 0;
   @Input() open = false;
   @Output() clicked = new EventEmitter<void>();
+  public readonly openIcon = faCaretDown;
+  public readonly closedIcon = faCaretUp;
 }
