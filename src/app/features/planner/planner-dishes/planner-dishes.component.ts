8e9ec60fe98e727d004a4895fdbc152d12e16d3c
John Coppola <jdcoppola@gmail.com>
2021-12-06 11:52:52 -0500

    extracts filtering logic into a dedicated service

--- src/app/features/planner/planner-dishes/planner-dishes.component.ts
+++ src/app/features/planner/planner-dishes/planner-dishes.component.ts
@@ -1,15 +1,14 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { BehaviorSubject, combineLatest } from 'rxjs';
-import { distinctUntilChanged, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 import { Dish } from '@models/interfaces/dish.interface';
 import { Menu } from '@models/interfaces/menu.interface';
 import { Day } from '@models/types/day.type';
-import { DishType } from '@models/types/dish-type.type';
 import { DishService } from '@services/dish.service';
+import { FilterService } from '@services/filter.service';
 import { MenuService } from '@services/menu.service';
 import { TagService } from '@services/tag.service';
-import { lower } from '@shared/utility/format';
 import { trackByFactory } from '@shared/utility/track-by-factory';
 
 @Component({
@@ -20,9 +19,6 @@ import { trackByFactory } from '@shared/utility/track-by-factory';
 })
 export class PlannerDishesComponent {
   private _menu$ = new BehaviorSubject<Menu | undefined>(undefined);
-  private _filterPanel$ = new BehaviorSubject<boolean>(false);
-  private _filters$ = new BehaviorSubject<string[]>([]);
-  private _searchText$ = new BehaviorSubject<string>('');
 
   @Input()
   public set menu(value: Menu | undefined) {
@@ -31,16 +27,18 @@ export class PlannerDishesComponent {
   public vm$ = combineLatest([
     this._menu$.asObservable(),
     this._dishService.getDishes(),
-    this._filterPanel$.asObservable(),
-    this._filters$.asObservable(),
-    this._searchText$.asObservable().pipe(
-      distinctUntilChanged(),
-    ),
     this._tagService.getTags(),
+    this._filterService.panel$,
+    this._filterService.tagIds$,
+    this._filterService.text$,
   ]).pipe(
-    map(([menu, dishes, filterPanel, filters, searchText, tags]) => {
-      const mains = dishes.filter(dish => this._filterDish(dish, 'main', searchText, filters));
-      const sides = dishes.filter(dish => this._filterDish(dish, 'side', searchText, filters));
+    map(([menu, dishes, tags, filterPanel, filters, searchText]) => {
+      const mains = dishes.filter(dish => this._filterService.filterDish({
+        dish, type: 'main', text: searchText, tagIds: filters,
+      }));
+      const sides = dishes.filter(dish => this._filterService.filterDish({
+        dish, type: 'side', text: searchText, tagIds: filters,
+      }));
       return {
         menu,
         tags,
@@ -57,12 +55,21 @@ export class PlannerDishesComponent {
 
   constructor(
     private _dishService: DishService,
+    private _filterService: FilterService,
     private _menuService: MenuService,
     private _tagService: TagService,
   ) { }
 
   public onSearchTextChange(text: string): void {
-    this._searchText$.next(text);
+    this._filterService.updateText(text);
+  }
+
+  public toggleFilterPanel(state: boolean): void {
+    this._filterService.updatePanel(!state);
+  }
+
+  public onFilterChange(filters: string[]): void {
+    this._filterService.updateTagIds(filters);
   }
 
   public async onDayChange(
@@ -78,21 +85,5 @@ export class PlannerDishesComponent {
       day,
       selected
     });
-  }
-
-  public toggleFilterPanel(state: boolean): void {
-    this._filterPanel$.next(!state);
-  }
-
-  public onFilterChange(filters: string[]): void {
-    this._filters$.next(filters);
-  }
-
-  private _filterDish(dish: Dish, type: DishType, searchText: string, filters: string[]): boolean {
-    return dish.type === type &&
-      (filters.length === 0 || dish.tags.some(tag => filters.includes(tag.id))) &&
-      (lower(dish.name).includes(lower(searchText)) ||
-      lower(dish.description).includes(lower(searchText)) ||
-      dish.tags.some(tag => lower(tag.name).includes(lower(searchText))));
   }
 }
