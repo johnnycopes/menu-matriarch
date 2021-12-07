04fbf722fa386beb010744fa7fbea3e7465c6474
John Coppola <jdcoppola@gmail.com>
2021-12-07 07:33:33 -0500

    adds 'Clear All' functionality to filters component

--- src/app/shared/domain/filters/filters.component.ts
+++ src/app/shared/domain/filters/filters.component.ts
@@ -13,7 +13,15 @@ export class FiltersComponent {
   @Input() tags: Tag[] = [];
   @Input() selected: string[] = [];
   @Output() selectedChange = new EventEmitter<string[]>();
+  @Output() selectedClear = new EventEmitter<void>();
   public trackByFn = trackByFactory<Tag, string>(tag => tag.id);
+
+  /*
+    TODO:
+    1. Create fallback text if no tags exist
+    2. Test how it looks/operates with a lot of tags
+    3. Refactor other tag displays to leverage column-gap/row-gap instead of margin
+  */
 
   public onTagChange(id: string, state: boolean): void {
     let updated: string[] = [];
