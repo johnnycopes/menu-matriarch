abe5084c7532775560c64ab60c522cbeda66fca0
John Coppola <jdcoppola@gmail.com>
2021-12-12 11:37:20 -0500

    updates meal to use FilteredDishesGroup interface

--- src/app/shared/domain/meal/meal.component.ts
+++ src/app/shared/domain/meal/meal.component.ts
@@ -1,15 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 import { Dish } from '@models/interfaces/dish.interface';
+import { FilteredDishesGroup } from '@models/interfaces/filtered-dishes.interface';
 import { DishType } from '@models/types/dish-type.type';
 import { getDishTypes } from '@models/types/get-dish-types';
 import { Orientation } from '@models/types/orientation.type';
 import { trackByFactory } from '@shared/utility/track-by-factory';
-
-interface DishesGroup {
-  type: DishType;
-  dishes: Dish[];
-}
 
 @Component({
   selector: 'app-meal',
@@ -28,8 +24,8 @@ export class MealComponent {
   }
   @Input() fallbackText = '';
   @Input() orientation: Orientation = 'horizontal';
-  public dishesGroups: DishesGroup[] = [];
+  public dishesGroups: FilteredDishesGroup[] = [];
   public showFallback = true;
-  public groupTrackByFn = trackByFactory<DishesGroup, DishType>(group => group.type);
+  public groupTrackByFn = trackByFactory<FilteredDishesGroup, DishType>(group => group.type);
   public dishTrackByFn = trackByFactory<Dish, string>(dish => dish.id);
 }
