808335c8feae0261b875bb44ade72d6e6da41cb8
John Coppola <jdcoppola@gmail.com>
2021-12-18 15:40:05 -0500

    sets up form UI

--- src/app/features/menus/menu-card/menu-card.component.ts
+++ src/app/features/menus/menu-card/menu-card.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faEllipsisV, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { merge, Subject } from 'rxjs';
 import { mapTo } from 'rxjs/operators';
 
@@ -7,7 +7,10 @@ import { MenuEntry } from '@models/menu-entry.interface';
 import { Orientation } from '@models/orientation.type';
 import { MenuService } from '@services/menu.service';
 import { PrintService } from '@services/print.service';
-import { trackByDay } from '@utility/domain/track-by-functions';
+import { getDays } from '@utility/domain/get-days';
+import { trackByDay, trackBySelf } from '@utility/domain/track-by-functions';
+
+type MenuCardAction = 'rename' | 'changeStartDay';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -23,8 +26,21 @@ export class MenuCardComponent {
   @Input() orientation: Orientation = 'horizontal';
   @Input() fallbackText = '';
   @Input() canDelete = true;
+  public readonly cancelIcon = faTimes;
   public readonly menuToggleIcon = faEllipsisV;
-  public readonly trackByFn = trackByDay;
+  public readonly saveIcon = faCheck;
+
+  public readonly days = getDays();
+  public readonly dayTrackByFn = trackBySelf;
+  public readonly menuEntryTrackByFn = trackByDay;
+
+  public startChangeStartDay$ = new Subject<MenuCardAction>();
+  public finishChangeStartDay$ = new Subject<void>();
+  public changing$ = merge(
+    this.startChangeStartDay$.pipe(mapTo(true)),
+    this.finishChangeStartDay$.pipe(mapTo(false)),
+  );
+
   public startRename$ = new Subject<void>();
   public finishRename$ = new Subject<void>();
   public renaming$ = merge(
