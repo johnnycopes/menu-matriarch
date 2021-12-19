dac0144168cc7780fb1489f66f9b193f4bac8d14
John Coppola <jdcoppola@gmail.com>
2021-12-19 16:39:33 -0500

    creates inline-form component and refactors existing inline components to use it

---
+++ src/app/shared/domain/inline-form/inline-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, ChangeDetectionStrategy, EventEmitter, Output, ViewEncapsulation, HostBinding } from '@angular/core';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-inline-form',
+  templateUrl: './inline-form.component.html',
+  styleUrls: ['./inline-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class InlineFormComponent {
+  @Output() cancel = new EventEmitter<void>();
+  @Output() save = new EventEmitter<void>();
+  public readonly cancelIcon = faTimes;
+  public readonly saveIcon = faCheck;
+
+  @HostBinding('class')
+  public hostClass = 'app-inline-form';
+}
