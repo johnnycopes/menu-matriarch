c3b0c296d67d18ba88758bec41282295bd363245
John Coppola <jdcoppola@gmail.com>
2021-12-19 15:32:41 -0500

    refactors menu-card logic to have centralized state subject

--- src/app/features/menus/menu-card/menu-card.component.ts
+++ src/app/features/menus/menu-card/menu-card.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
-import { merge, Subject } from 'rxjs';
-import { mapTo } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 import { Day } from '@models/day.type';
 import { MenuEntry } from '@models/menu-entry.interface';
@@ -10,7 +9,7 @@ import { MenuService } from '@services/menu.service';
 import { PrintService } from '@services/print.service';
 import { trackByDay } from '@utility/domain/track-by-functions';
 
-type MenuCardAction = 'rename' | 'changeStartDay';
+type State = 'default' | 'renaming' | 'changingStartDay';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -27,22 +26,10 @@ export class MenuCardComponent {
   @Input() orientation: Orientation = 'horizontal';
   @Input() fallbackText = '';
   @Input() canDelete = true;
+  private _state$ = new BehaviorSubject<State>('default');
+  public state$ = this._state$.asObservable();
   public readonly menuToggleIcon = faEllipsisV;
   public readonly trackByFn = trackByDay;
-
-  public startChangeStartDay$ = new Subject<MenuCardAction>();
-  public finishChangeStartDay$ = new Subject<void>();
-  public changing$ = merge(
-    this.startChangeStartDay$.pipe(mapTo(true)),
-    this.finishChangeStartDay$.pipe(mapTo(false)),
-  );
-
-  public startRename$ = new Subject<void>();
-  public finishRename$ = new Subject<void>();
-  public renaming$ = merge(
-    this.startRename$.pipe(mapTo(true)),
-    this.finishRename$.pipe(mapTo(false)),
-  );
 
   constructor(
     private _menuService: MenuService,
@@ -58,14 +45,26 @@ export class MenuCardComponent {
     });
   }
 
-  public async onRename(name: string): Promise<void> {
-    await this._menuService.updateMenuName(this.id, name);
-    this.finishRename$.next();
+  public onRename(): void {
+    this._state$.next('renaming');
   }
 
-  public async onChangeStartDay(startDay: Day): Promise<void> {
+  public async onRenameSave(name: string): Promise<void> {
+    await this._menuService.updateMenuName(this.id, name);
+    this._state$.next('default');
+  }
+
+  public onChangeStartDay(): void {
+    this._state$.next('changingStartDay');
+  }
+
+  public async onChangeStartDaySave(startDay: Day): Promise<void> {
     await this._menuService.updateMenuStartDay(this.id, startDay);
-    this.finishChangeStartDay$.next();
+    this._state$.next('default');
+  }
+
+  public onCancel(): void {
+    this._state$.next('default');
   }
 
   public onDelete(): void {
