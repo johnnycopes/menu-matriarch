ac1b1abf454da14607ff02f0bc0cb38e574deaf8
John Coppola <jdcoppola@gmail.com>
2021-12-19 16:47:55 -0500

    moves inline-form to shared generic module

--- src/app/shared/domain/domain.module.ts
+++ src/app/shared/domain/domain.module.ts
@@ -11,7 +11,6 @@ import { DishSummaryComponent } from './dish-summary/dish-summary.component';
 import { EmptyViewPlaceholderComponent } from './empty-view-placeholder/empty-view-placeholder.component';
 import { FiltersButtonComponent } from './filters-button/filters-button.component';
 import { FiltersComponent } from './filters/filters.component';
-import { InlineFormComponent } from './inline-form/inline-form.component';
 import { InlineDaySelectComponent } from './inline-day-select/inline-day-select.component';
 import { InlineNameEditComponent } from './inline-name-edit/inline-name-edit.component';
 import { MealComponent } from './meal/meal.component';
@@ -26,7 +25,6 @@ import { TagComponent } from './tag/tag.component';
     EmptyViewPlaceholderComponent,
     FiltersButtonComponent,
     FiltersComponent,
-    InlineFormComponent,
     InlineDaySelectComponent,
     InlineNameEditComponent,
     MealComponent,
