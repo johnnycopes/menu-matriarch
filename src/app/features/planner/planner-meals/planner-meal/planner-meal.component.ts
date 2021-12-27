0c582ef813349211b4d4290bb7a508b5ab423315
John Coppola <jdcoppola@gmail.com>
2021-12-27 08:50:44 -0500

    begins writing logic to add/remove meal from a planner day

--- src/app/features/planner/planner-meals/planner-meal/planner-meal.component.ts
+++ src/app/features/planner/planner-meals/planner-meal/planner-meal.component.ts
@@ -1,8 +1,16 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import { Day } from '@models/day.type';
 
 import { Dish } from '@models/dish.interface';
+import { Menu } from '@models/menu.interface';
 import { Orientation } from '@models/orientation.type';
 import { Tag } from '@models/tag.interface';
+import { trackByDay } from '@utility/domain/track-by-functions';
+
+interface EntryModel {
+  day: Day;
+  checked: boolean;
+}
 
 @Component({
   selector: 'app-planner-meal',
@@ -16,7 +24,21 @@ export class PlannerMealComponent {
   @Input() description = '';
   @Input() dishes: Dish[] = [];
   @Input() tags: Tag[] = [];
-  @Input() active = false;
   @Input() fallbackText = '';
   @Input() orientation: Orientation = 'horizontal';
+  @Input()
+  public set menu(menu: Menu | undefined) {
+    this.entryModels = menu?.entries.map(entry => {
+      return {
+        day: entry.day,
+        // TODO: write correct states in model
+        // checked: !!entry.dishes.find(dish => dish.id === this.id),
+        checked: false,
+      };
+    }) ?? [];
+  };
+  @Output() dayChange = new EventEmitter<{ dishes: Dish[], day: Day, selected: boolean }>();
+
+  public entryModels: EntryModel[] = [];
+  public readonly trackByFn = trackByDay;
 }
--- src/app/features/planner/planner-meals/planner-meals.component.ts
+++ src/app/features/planner/planner-meals/planner-meals.component.ts
@@ -1,6 +1,9 @@
 import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 
+import { Day } from '@models/day.type';
+import { Dish } from '@models/dish.interface';
 import { Menu } from '@models/menu.interface';
+import { MenuService } from '@services/menu.service';
 
 @Component({
   selector: 'app-planner-meals',
@@ -11,4 +14,23 @@ import { Menu } from '@models/menu.interface';
 export class PlannerMealsComponent {
   @Input() menu: Menu | undefined;
   @Output() nameDblClick = new EventEmitter<void>();
+
+  constructor (private _menuService: MenuService) { }
+
+  public async onDayChange(
+    menu: Menu | undefined,
+    { dishes, day, selected }: { dishes: Dish[], day: Day, selected: boolean }
+  ): Promise<void> {
+    if (!menu) {
+      return;
+    }
+    console.log({ dishes, day, selected });
+    // TODO: update menuService to handle multiple dish updates at once
+    // return this._menuService.updateMenuContents({
+    //   menu,
+    //   dishId: id,
+    //   day,
+    //   selected
+    // });
+  }
 }
