ae6e3bd997ae025f28b678c0a703eef495998a25
John Coppola <jdcoppola@gmail.com>
2022-01-05 19:36:27 -0500

    creates user-document service

---
+++ src/app/core/services/user-document.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Observable } from 'rxjs';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
+
+import { UserDto } from '@models/dtos/user-dto.interface';
+import { Endpoint } from '@models/endpoint.enum';
+import { User } from '@models/user.interface';
+import { UserPreferences } from '@models/user-preferences.interface';
+import { FirestoreService } from './firestore.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDocumentService {
+  private _endpoint = Endpoint.users;
+
+  constructor(
+    private _auth: AngularFireAuth,
+    private _firestoreService: FirestoreService,
+  ) { }
+
+  public get uid$() {
+    return this._auth.user.pipe(
+      map(user => user?.uid),
+      shareReplay({ refCount: true, bufferSize: 1 }),
+    );
+  }
+
+  public getUser(): Observable<User | undefined> {
+    return this.uid$.pipe(
+      switchMap(uid => this._firestoreService.getOne<UserDto>(this._endpoint, uid)),
+    );
+  }
+
+  public getPreferences(): Observable<UserPreferences | undefined> {
+    return this.getUser().pipe(
+      map(user => user?.preferences),
+    );
+  }
+
+  public updatePreferences(uid: string, preferences: UserPreferences): Promise<void> {
+    return this._firestoreService.update<UserDto>(
+      this._endpoint,
+      uid,
+      { preferences }
+    );
+  }
+}
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,60 +1,28 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Observable } from 'rxjs';
-import { first, map, shareReplay, switchMap, tap } from 'rxjs/operators';
+import { first, tap } from 'rxjs/operators';
 
-import { Endpoint } from '@models/endpoint.enum';
-import { createUserDto } from '@shared/utility/domain/create-dtos';
-import { UserDto } from '@models/dtos/user-dto.interface';
 import { User } from '@models/user.interface';
 import { UserPreferences } from '@models/user-preferences.interface';
-import { FirestoreService } from './firestore.service';
+import { UserDocumentService } from './user-document.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private _endpoint = Endpoint.users;
 
-  constructor(
-    private _auth: AngularFireAuth,
-    private _firestoreService: FirestoreService,
-  ) { }
+  constructor(private _userDocumentService: UserDocumentService) { }
 
-  public get uid$() {
-    return this._auth.user.pipe(
-      map(user => user?.uid),
-      shareReplay({ refCount: true, bufferSize: 1 }),
-    );
+  public get uid$(): Observable<string | undefined> {
+    return this._userDocumentService.uid$;
   }
 
   public getUser(): Observable<User | undefined> {
-    return this.uid$.pipe(
-      switchMap(uid => this._firestoreService.getOne<UserDto>(this._endpoint, uid)),
-    );
-  }
-
-  public createUser(
-    user: Partial<Omit<UserDto, 'uid'>>
-  ): Observable<string | undefined> {
-    return this.uid$.pipe(
-      first(),
-      tap(async uid => {
-        if (uid) {
-          await this._firestoreService.create<UserDto>(
-            this._endpoint,
-            uid,
-            createUserDto({ uid, ...user }),
-          );
-        }
-      })
-    );
+    return this._userDocumentService.getUser();
   }
 
   public getPreferences(): Observable<UserPreferences | undefined> {
-    return this.getUser().pipe(
-      map(user => user?.preferences),
-    );
+    return this._userDocumentService.getPreferences();
   }
 
   public updatePreferences(updates: Partial<UserPreferences>): Observable<User | undefined> {
@@ -71,17 +39,14 @@ export class UserService {
           emptyMealText: 'undecided',
           mealOrientation: 'horizontal',
         };
-        await this._firestoreService.update<UserDto>(
-          this._endpoint,
+        await this._userDocumentService.updatePreferences(
           user.uid,
-          { preferences:
-            {
-              darkMode: updates?.darkMode ?? fallbackPreferences.darkMode,
-              dayNameDisplay: updates?.dayNameDisplay ?? fallbackPreferences.dayNameDisplay,
-              defaultMenuStartDay: updates?.defaultMenuStartDay ?? fallbackPreferences.defaultMenuStartDay,
-              emptyMealText: updates?.emptyMealText ?? fallbackPreferences.emptyMealText,
-              mealOrientation: updates?.mealOrientation ?? fallbackPreferences.mealOrientation,
-            },
+          {
+            darkMode: updates?.darkMode ?? fallbackPreferences.darkMode,
+            dayNameDisplay: updates?.dayNameDisplay ?? fallbackPreferences.dayNameDisplay,
+            defaultMenuStartDay: updates?.defaultMenuStartDay ?? fallbackPreferences.defaultMenuStartDay,
+            emptyMealText: updates?.emptyMealText ?? fallbackPreferences.emptyMealText,
+            mealOrientation: updates?.mealOrientation ?? fallbackPreferences.mealOrientation,
           }
         );
       }),
