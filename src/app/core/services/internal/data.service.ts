2ce9219a1b15c7e8bf4cc2bcc89247584a69bd1f
John Coppola <jdcoppola@gmail.com>
2022-01-16 09:08:45 -0700

    removes unused import from dataService

--- src/app/core/services/internal/data.service.ts
+++ src/app/core/services/internal/data.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { Batch } from './batch';
 import { FirestoreService } from './firestore.service';
 
 @Injectable({
