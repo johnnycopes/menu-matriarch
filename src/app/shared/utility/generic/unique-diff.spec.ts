df39619b1f379a5919ed57961489aec8d8062fc9
John Coppola <jdcoppola@gmail.com>
2022-02-01 19:02:45 -0500

    extracts uniqueDiff function

---
+++ src/app/shared/utility/generic/unique-diff.spec.ts
@@ -0,0 +1,27 @@
+import { uniqeDiff } from "./unique-diff";
+
+describe('uniqueDiff', () => {
+  it('returns empty values when initial and final are empty', () => {
+    expect(uniqeDiff([], [])).toEqual({ added: [], removed: [] });
+  });
+
+  it('returns empty values when initial and final are equal', () => {
+    expect(uniqeDiff([1, 2, 3], [1, 2, 3])).toEqual({ added: [], removed: [] });
+  });
+
+  it('returns removed values when initial has unique values', () => {
+    expect(uniqeDiff([1, 2, 3], [1, 2])).toEqual({ added: [], removed: [3] });
+  });
+
+  it('returns added values when final has unique values', () => {
+    expect(uniqeDiff([1, 2], [1, 2, 3])).toEqual({ added: [3], removed: [] });
+  });
+
+  it('returns empty values despite duplicates', () => {
+    expect(uniqeDiff([1, 1, 3, 2, 2], [3, 3, 2, 1, 1])).toEqual({ added: [], removed: [] });
+  });
+
+  it('returns correct values despite duplicates', () => {
+    expect(uniqeDiff([1, 1, 1, 2], [3, 3, 3, 4])).toEqual({ added: [3, 4], removed: [1, 2] });
+  });
+});
